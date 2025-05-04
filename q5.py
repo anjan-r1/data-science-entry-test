a9c13042ad04fd25a6669a9522cf62efbd91be22
anjan-r1 <anjankumar.tcs@gmail.com>
2025-05-04 23:12:23 +0800

    Update q5.py

--- q5.py
+++ q5.py
@@ -1,14 +1,25 @@
 def check_divisibility(num, divisor):
     """
-    Task 1
-    - Create a function to check if the number (num) is divisible by another number (divisor).
+    - A function to check if the number (num) is divisible by another number (divisor).
     - Both num and divisor must be numeric.
     - Return True if num is divisible by divisor, False otherwise.
     """
+    if type(num) is int and type(divisor) is int:
+      if num % divisor == 0:
+         return True
+      else:
+         return False
+    else:
+      return "Error: Invalid number"
     return
 
-
-# Task 2
 # Invoke the function "check_divisibility" using the following scenarios:
-# - 10, 2
-# - 7, 3
+# Scenario 1:
+print("Scenario 1:")
+print(check_divisibility(10, 2))
+
+print("")
+
+# Scenario 2:
+print("Scenario 2:")
+print(check_divisibility(7, 3))
